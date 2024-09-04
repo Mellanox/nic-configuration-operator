c5b6f3cd7829cd2be1fa2f27e58bb303c8bfc7e1
amaslennikov <amaslennikov@nvidia.com>
2024-09-04 14:25:28 +0300

    Add implementation of NicConfigurationTemplate reconciler
    This reconciler is getting a list of NicDevice matching the selectors
    and applies the template spec to them
    
    Signed-off-by: amaslennikov <amaslennikov@nvidia.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -21,6 +21,7 @@ import (
 	"flag"
 	"os"
 
+	"github.com/Mellanox/nic-configuration-operator/pkg/ncolog"
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -30,7 +31,6 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
@@ -66,13 +66,9 @@ func main() {
 		"If set the metrics endpoint is served securely")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
+	ncolog.BindFlags(flag.CommandLine)
 	flag.Parse()
-
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	ncolog.InitLog()
 
 	// if the enable-http2 flag is false (the default), http/2 should be disabled
 	// due to its vulnerabilities. More specifically, disabling http/2 will
